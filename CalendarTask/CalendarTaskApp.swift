d7d55ef035087fdc3c3468cf5573614c0f20cafe
Mouygech Leang <leangmouygech68@gmail.com>
2022-05-21 13:17:28 +0700

    Initial Commit

---
+++ CalendarTask/CalendarTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarTaskApp.swift
+//  CalendarTask
+//
+//  Created by Leng Mouyngech on 21/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CalendarTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
